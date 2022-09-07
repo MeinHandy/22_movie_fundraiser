065a8aef2885de48691025b821f12f1bb1411507
ah19017 <ah19017@melville-high.school.nz>
2022-09-08 11:54:06 +1200

    01_not_blank.py now has comments and variable "q" is now named the better "questions as it is more descriptive"

--- 01_not_blank.py
+++ 01_not_blank.py
@@ -1,13 +1,13 @@
 
-def not_blank(q):
+def not_blank(question):
     while True:
-        answer = input(q).strip()
+        answer = input(question).strip()  # saves the input to "answer" variable, strips whitespace at the start or end of input
 
-        if answer == "":
-            print("Input cannot be blank.")
+        if answer == "":  # checks if the answer is blank
+            print("Input cannot be blank.")  # prints an error based on what has one wrong
         else:
-            return answer
+            return answer  # returns the answer
 
 
-name = not_blank("What is your name?: ")
-print("Hello {}".format(name))
+name = not_blank("What is your name?: ")  # triggers not_blank with the input of question
+print("Hello {}".format(name))  # greets user by printing the name
