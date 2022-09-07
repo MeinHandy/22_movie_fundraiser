53e535bd3ad7209479728f9b6532bc7a8f55e8d4
ah19017 <ah19017@melville-high.school.nz>
2022-09-08 11:44:00 +1200

    03_number_check.py is complete and ready to be used

--- 03_number_check.py
+++ 03_number_check.py
@@ -1,14 +1,17 @@
-while True:
-    try:
-        age = int(input("how old nerd? "))
-    except ValueError:
-        print("Please input a whole number")
-        continue
-    break
+def age_check():
+    while True:
+        try:
+            age = int(input("How old are you? "))
+        except ValueError:
+            print("Please input a whole number")
+            continue
+        if age < 12:
+            print("You are too young to view the movie")
+        elif age > 113:
+            print("This age is fake, try again")
+        else:
+            print("Welcome")
+            break
 
-if age < 12:
-    print("You are too young to view the movie")
-elif age > 113:
-    print("This age is fake, try again")
-else:
-    print("Welcome")
+
+age_check()
