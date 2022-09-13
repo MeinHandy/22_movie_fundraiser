1085c761a8b7b869f72232292554ffb617d680e8
ah19017 <ah19017@melville-high.school.nz>
2022-09-14 09:36:31 +1200

    updated main.py, inputting an age too young now returns to age question to make it harder to evade age restrictions

--- main.py
+++ main.py
@@ -10,17 +10,16 @@ def not_blank(question):
 
 
 # 02_get_input
-def seat_counter():
-    available_seats = 150  # declares the number of available seats
+def seat_counter(available_seats):
     while available_seats > 0:  # while loops the code when the available seats is more than 0
         print('There are {} seats remaining'.format(available_seats))  # prints the total number of remaining seats
         name = not_blank("What is your name?: ")  # uses the not_blank function so name is not blank
-        available_seats -= 1  # reduces available seats by one each time
         print("Hello {}".format(name))  # prints greets user as a sign that the name has been accepted.
-        age_check()
+        available_seats = age_check(available_seats)
 
 
-def age_check():
+
+def age_check(available_seats):
     while True:  # loops until break
         try:
             age = int(input("How old are you? "))
@@ -29,10 +28,12 @@ def age_check():
             continue  # restarts the loop
         if age < 12:  # if/elif/else for age logic
             print("You are too young to view the movie")
+            seat_counter(available_seats)
         elif age > 113:
             print("This age is fake, try again")
         else:
             print("Welcome")
-            break  # exits the while loop as input was accepted
-
-seat_counter()
+            available_seats -= 1  # reduces available seats by one each time
+            return available_seats
+available_seats = 150 # declares the number of available seats
+seat_counter(available_seats)
