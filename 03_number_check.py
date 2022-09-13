b823b2eb182b4f86db552be9893f7e3700cb3ac3
ah19017 <ah19017@melville-high.school.nz>
2022-09-14 09:05:30 +1200

    03_number_check.py comments

--- 03_number_check.py
+++ 03_number_check.py
@@ -1,17 +1,17 @@
 def age_check():
-    while True:
+    while True:  # loops until break
         try:
             age = int(input("How old are you? "))
-        except ValueError:
+        except ValueError:  # confirms age is an integer
             print("Please input a whole number")
-            continue
-        if age < 12:
+            continue  # restarts the loop
+        if age < 12:  # if/elif/else for age logic
             print("You are too young to view the movie")
         elif age > 113:
             print("This age is fake, try again")
         else:
             print("Welcome")
-            break
+            break  # exits the while loop as input was accepted
 
 
-age_check()
+age_check()  # runs the age check
