7228f0b046d8cd2943fa7d79d9bac7166a272e82
ah19017 <ah19017@melville-high.school.nz>
2022-09-21 09:44:48 +1200

    05-06_string_check.py is finished, 05-06 because 05 is a simple version but will be upgraded to a more advanced version which is 06_string_check

---
+++ 05-06_string_check.py
@@ -0,0 +1,67 @@
+# 01_not_blank
+available_seats = 150
+
+
+def not_blank(question):
+    while True:  # loops indefinitely until an acceptable input is received
+        answer = input(question).strip()  # saves the input to "answer" variable, strips whitespace at the start or end of input
+
+        if answer == "":  # checks if the answer is blank
+            print("Input cannot be blank.")  # prints an error based on what has one wrong
+        else:
+            return answer  # returns the answer
+
+
+# 02_get_input
+def seat_counter(seatage):
+    seatage -= 1  # reduces available seats by one each time
+    return seatage
+
+
+def age_check():
+    while True:  # loops until break
+        try:
+            age = int(input("How old are you? "))
+        except ValueError:  # confirms age is an integer
+            print("Please input a whole number")
+            continue  # restarts the loop
+        if age < 12:  # if/elif/else for age logic
+            print("You are too young to view the movie")
+        elif age > 113:
+            print("This age is fake, try again")
+        else:
+            return age
+
+
+def ticket_price(user_age):
+    ticket_total = 0
+    if user_age <= 15:
+        ticket_total += 7.5
+    elif age <= 64:
+        ticket_total += 10.5
+    elif age >= 65:
+        ticket_total += 6.5
+    else:
+        print("error occurred, unaccounted for age recieved")
+    print("cost total is ${:.2f}".format(ticket_total))
+    return ticket_total
+
+
+def string_checker(question, for_check):
+    valid = False
+    while not valid:
+        answer = input(question).strip().lower()
+        if answer in for_check:
+            valid = True
+            return answer
+        else:
+            print("Invalid Input, please answer Yes or No")
+
+
+while available_seats > 0:  # while loops the code when the available seats is more than 0
+    print('There are {} seats remaining'.format(available_seats))  # prints the total number of remaining seats
+    name = not_blank("What is your name?: ")  # uses the not_blank function so name is not blank
+    available_seats = seat_counter(available_seats)  # runs seat_counter
+    age = age_check()
+    total_cost = ticket_price(age)
+    wants_snacks = string_checker("Do you want snacks for the movie? (Yes/No) ", ["yes", "no"])
